6bbf7f63e25f9599e774c075e9616b882a6bd60d
Sabari Vel <sabarivel0928@gmail.com>
2025-07-04 17:14:53 +0530

    Delete Click.js

--- Click.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const ClickSchema = new mongoose.Schema({
-  shortcode: String,
-  timestamp: { type: Date, default: Date.now },
-  referrer: String,
-  location: Object,
-});
-module.exports = mongoose.model('Click', ClickSchema);
