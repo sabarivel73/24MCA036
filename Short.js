a8216ee3dc23e137dd97fa6ea0138f5aec367176
Sabari Vel <sabarivel0928@gmail.com>
2025-07-04 17:15:29 +0530

    Delete Short.js

--- Short.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const ShortUrlSchema = new mongoose.Schema({
-  shortcode: { type: String, unique: true, required: true },
-  originalUrl: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  expiry: { type: Date, required: true }
-});
-module.exports = mongoose.model('ShortUrl', ShortUrlSchema);
